f8480cdcd2befea17e0f99257756e6eb80af06b0
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-09 01:06:23 +0900

    using vercel url for api

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,35 +18,8 @@ const article = ({ article }) => {
     )
 }
 
-// export const getStaticProps = async (context) => {
-//     const res = await fetch(`${server}/api/articles/${context.params.id}`)
-
-//     const article = await res.json()
-
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-// }
-
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`${server}/api/articles`)
-
-//     const articles = await res.json()
-
-//     const ids = articles.map(article => article.id)
-//     const paths = ids.map(id => ({ params: { id: id.toString() } }))
-
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
-
-
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
 
@@ -58,7 +31,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
 
     const articles = await res.json()
 
@@ -70,5 +43,32 @@ export const getStaticPaths = async () => {
         fallback: false
     }
 }
+
+
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//     const articles = await res.json()
+
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({ params: { id: id.toString() } }))
+
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
 export default article
