0e3c895be9e4257079798a6315dd53d384deafc1
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-09 01:03:14 +0900

    change index id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,31 +18,31 @@ const article = ({ article }) => {
     )
 }
 
-export const getStaticProps = async (context) => {
-    const res = await fetch(`${server}/api/articles/${context.params.id}`)
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
-    const article = await res.json()
+//     const article = await res.json()
 
-    return {
-        props: {
-            article
-        }
-    }
-}
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
 
-export const getStaticPaths = async () => {
-    const res = await fetch(`${server}/api/articles`)
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`${server}/api/articles`)
 
-    const articles = await res.json()
+//     const articles = await res.json()
 
-    const ids = articles.map(article => article.id)
-    const paths = ids.map(id => ({ params: { id: id.toString() } }))
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({ params: { id: id.toString() } }))
 
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
 
 export const getStaticProps = async (context) => {
