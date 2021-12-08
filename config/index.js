1537343d063828642a01f6602624c6fbd093c737
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-09 00:42:38 +0900

    change api

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+// export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+export const server = dev ? 'https://learn-nextjs-crash-course-3.vercel.app' : 'https://yourwebsite.com'
