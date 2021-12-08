7fef6b875ea0e20a0d6c976a766a48a928d92ca3
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-12-09 01:38:57 +0900

    remove image functions

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -2,11 +2,13 @@ import Link from 'next/link'
 import articleStyles from '../styles/Article.module.css'
 
 const ArticleList = ({ article }) => {
+
     return (
         <Link href="/article/[id]" as={`/article/${article.id}`}>
             <a className={articleStyles.card}>
                 <h3>{article.title} &rarr;</h3>
                 <p>{article.excerpt ? article.excerpt : article.body}</p>
+                <i>{article.url}</i>
             </a>
         </Link>
     )
